228b1891c20e30c3fe1f84d54b775da27cac1003
carsonmorris <carsonmorris548@u.boisestate.edu>
2022-10-13 09:33:22 -0600

    Typescript working?

--- build/__tests__/test.app.js
+++ build/__tests__/test.app.js
@@ -1,2 +1,8 @@
-"use strict";
+import { describe, expect, test } from '@jest/globals';
+import { hello } from '../app';
+describe('sum module', function () {
+    test('returns the string hello', function () {
+        expect(hello()).toBe('Hello World');
+    });
+});
 //# sourceMappingURL=test.app.js.map
