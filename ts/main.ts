0239780ed28561a29ef0c67c2c779397215a0827
carsonmorris <carsonmorris548@u.boisestate.edu>
2022-10-13 09:14:41 -0600

    Testing Typescript stuff

--- ts/main.ts
+++
@@ -1,9 +0,0 @@
-window.onload = loaded;
-
-/**
- * Simple Function that will be run when the browser is finished loading.
- */
-function loaded() {
-    let message = "Hello world";
-    console.log("Loaded main.ts:" + message);
-}
